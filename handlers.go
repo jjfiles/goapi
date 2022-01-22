718b10e1a0dc11566d04fe7cdf2c5092fcdc70f1
jjfiles <jeffery@jfiles.dev>
2022-01-22 14:43:37 -0600

    added a new test route and handler

--- handlers.go
+++ handlers.go
@@ -8,3 +8,7 @@ import (
 func BaseHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 }
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(map[string]bool{"ok": false})
+}
--- routes.go
+++ routes.go
@@ -5,5 +5,6 @@ import "github.com/gorilla/mux"
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", BaseHandler)
+	r.HandleFunc("/test", TestHandler)
 	return r
 }
